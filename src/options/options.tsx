036ffca8baa8cf4c77254c7f2745d2a475620064
Zaif Senpai <escbutt@gmail.com>
2024-01-03 00:48:08 +0500

    Reformatted code
    Updated contents of options and popup to use mui components

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './options.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./options.css";
+import {
+  Box,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 
 const App: React.FC<{}> = () => {
-  return (
-    <div>
-      <img src="icon.png" />
-    </div>
-  )
-}
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
 
-const container = document.createElement('div')
-document.title = "Options"
-document.body.appendChild(container)
-const root = createRoot(container)
-root.render(<App />)
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Box
+        sx={{
+          m: "10px",
+        }}
+      >
+        <Typography>Hello from Options</Typography>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+const container = document.createElement("div");
+document.title = "Options";
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<App />);
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './popup.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./popup.css";
+import {
+  Box,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 
 const App: React.FC<{}> = () => {
-  return (
-    <div>
-      <img src="icon.png" />
-    </div>
-  )
-}
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
 
-const container = document.createElement('div')
-document.title = "Options"
-document.body.appendChild(container)
-const root = createRoot(container)
-root.render(<App />)
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Box
+        sx={{
+          m: "10px",
+        }}
+      >
+        <Typography>Hello from Popup</Typography>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+const container = document.createElement("div");
+document.title = "Options";
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<App />);
